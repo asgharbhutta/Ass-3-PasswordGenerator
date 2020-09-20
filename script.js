b953d96795d0b015f5a033615a46e198c04901c5
AsgharB <abhutta@Asghar.local>
2020-09-19 21:29:30 -0400

    added loops for conditions, less then 7 and higher then 128, made it so atleast 1 type must be selected

--- script.js
+++ script.js
@@ -18,43 +18,47 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var lenOfPassword = 7;
+  
+while (lenOfPassword <= 7 || lenOfPassword >= 129){  
   var lenOfPassword = window.prompt("How long do you want the password to be?");
+}
+
   var fullCharacters = "";
 
-while(fullCharacters == ""){  
-  var lowCharTrue = window.confirm("Add lower case letters?");
-  if(lowCharTrue){
-    fullCharacters += lowChar;
-  }else{
-    fullCharacters += "";
+  while(fullCharacters == ""){  
+    var lowCharTrue = window.confirm("Add lower case letters?");
+    if(lowCharTrue){
+      fullCharacters += lowChar;
+    }else{
+      fullCharacters += "";
+    }
+
+    var upperCharTrue = window.confirm("Add upper case letters?");
+    if(upperCharTrue){
+      fullCharacters += upperChar;
+    }else{
+      fullCharacters += "";
+    }
+
+    var numTrue = window.confirm("Add numbers?");
+    if(numTrue){
+      fullCharacters += num;
+    }else{
+      fullCharacters += ""
+    }
+
+    var symTrue = window.confirm("Add symbols?");
+    if(symTrue){
+      fullCharacters += sym;
+    }else{
+      fullCharacters += "";
+    }
   }
+    var password = generatePassword(lenOfPassword, fullCharacters);
+    var passwordText = document.querySelector("#password");
 
-  var upperCharTrue = window.confirm("Add upper case letters?");
-  if(upperCharTrue){
-    fullCharacters += upperChar;
-  }else{
-    fullCharacters += "";
-  }
-
-  var numTrue = window.confirm("Add numbers?");
-  if(numTrue){
-    fullCharacters += num;
-  }else{
-    fullCharacters += ""
-  }
-
-  var symTrue = window.confirm("Add symbols?");
-  if(symTrue){
-    fullCharacters += sym;
-  }else{
-    fullCharacters += "";
-  }
-}
-  var password = generatePassword(lenOfPassword, fullCharacters);
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+    passwordText.value = password;
 }
 
 // Add event listener to generate button
