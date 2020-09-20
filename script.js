e2909aaf38339c4deda4dc59979d64d0cb141a42
AsgharB <abhutta@Asghar.local>
2020-09-20 10:45:52 -0400

    Added comments, fixed spacing, easier to read the code and displayed generated password on the webpage

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// Assignment code here
+//Variables that hold character sets
 var lowChar = "abcdefghijklmnopqrstuvwxyz";
 var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var num = "0123456789";
 var sym = "!@#$%^&*()_-+=";
-
+//Function to generate password, len = length of password inputted by prompt
+//characters = character set being used, in this case fullCharacters
 function generatePassword(len,characters){
   var pwd = "";
-
   for(var i = 0; i < len; i++){
     pwd += characters.charAt(Math.floor(Math.random() * characters.length));
   }
@@ -18,14 +18,16 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var lenOfPassword = 7;
-  
+
+//Setting initial value of 7 to ensure password length cannot be lower than 8
+//or higher than 128 characters
+var lenOfPassword = 7;
 while (lenOfPassword <= 7 || lenOfPassword >= 129){  
   var lenOfPassword = window.prompt("How long do you want the password to be?");
 }
-
-  var fullCharacters = "";
-
+//Setting an empty string to accept possible values
+var fullCharacters = "";
+  //While loop until fullCharacters has atleast 1 character set added
   while(fullCharacters == ""){  
     var lowCharTrue = window.confirm("Add lower case letters?");
     if(lowCharTrue){
@@ -54,7 +56,8 @@ while (lenOfPassword <= 7 || lenOfPassword >= 129){
     }else{
       fullCharacters += "";
     }
-  }
+  }//while looper end
+    //changed name to generate password function
     var password = generatePassword(lenOfPassword, fullCharacters);
     var passwordText = document.querySelector("#password");
 
